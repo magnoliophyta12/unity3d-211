d321330a0db294088682eab42663da3b489e9cff
flowermaiden <predictormagnolia@gmail.com>
2024-11-26 11:45:34 +0200

    hw4

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -13,12 +13,15 @@ public class CameraScript : MonoBehaviour
     private float minVerticalAngle = 35.0f; 
     private float maxVerticalAngle = 75.0f;
     private float minFpvDistance = 0.9f;
+    private float maxFpvDistance = 15.0f;
+    private bool isPos3;
     void Start()
     {
         lookAction = InputSystem.actions.FindAction("Look");
         cameraAngles = this.transform.eulerAngles;
         character = GameObject.Find("Character").transform;
         r=this.transform.position-character.position;
+        isPos3 =false;
 
     }
 
@@ -28,39 +31,56 @@ public class CameraScript : MonoBehaviour
         Vector2 wheel = Input.mouseScrollDelta;
         if (wheel.y != 0)
         {
-            if (r.magnitude > minFpvDistance)
+            if (r.magnitude >= maxFpvDistance)
             {
-                float rr = r.magnitude * (1 - wheel.y / 10);
-                if (rr <= minFpvDistance) 
+                isPos3 = true;
+                if (wheel.y < 0)
                 {
-                    r *= 0.01f;
-                }
-                else
-                {
-                    r *= (1 - wheel.y / 10);
+                    r *= 1 - wheel.y / 10;
                 }
             }
             else
             {
-                if (wheel.y < 0)
+                isPos3 = false;
+                if (r.magnitude >= minFpvDistance)
                 {
-                    r *= 100f;
+                    float rr = r.magnitude * (1 - wheel.y / 10);
+                    if (rr <= minFpvDistance)
+                    {
+                        r *= 0.01f;
+                    }
+                    else
+                    {
+                        r *= (1 - wheel.y / 10);
+                    }
                 }
+                else
+                {
+                    if (wheel.y < 0)
+                    {
+                        r *= 100f;
+                    }
+                }
+
+               // r *= 1 - wheel.y / 10;
             }
-
-            r *= 1 - wheel.y / 10;
+          
         }
-        Vector2 lookValue=lookAction.ReadValue<Vector2>();
-        if(lookValue != Vector2.zero)
+        if (!isPos3)
         {
-            cameraAngles.x += lookValue.y * Time.deltaTime * sensitivityH;
-            cameraAngles.y += lookValue.x * Time.deltaTime * sensitivityH;
+            Vector2 lookValue = lookAction.ReadValue<Vector2>();
+            if (lookValue != Vector2.zero)
+            {
+                cameraAngles.x += lookValue.y * Time.deltaTime * sensitivityH;
+                cameraAngles.y += lookValue.x * Time.deltaTime * sensitivityH;
 
-            cameraAngles.x = Mathf.Clamp(cameraAngles.x, minVerticalAngle, maxVerticalAngle);
+                cameraAngles.x = Mathf.Clamp(cameraAngles.x, minVerticalAngle, maxVerticalAngle);
 
-            this.transform.eulerAngles = cameraAngles;
+                this.transform.eulerAngles = cameraAngles;
 
+            }
+            this.transform.position = character.position + Quaternion.Euler(0, cameraAngles.y, 0) * r;
         }
-        this.transform.position = character.position+Quaternion.Euler(0,cameraAngles.y,0) * r;
+       
     }
 }
--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -16,10 +16,25 @@ public class CharacterScript : MonoBehaviour
     void Update()
     {
         Vector2 moveValue=moveAction.ReadValue<Vector2>();
+
+        //Projections:
+        Vector3 f=Camera.main.transform.forward;
+        f.y = 0.0f;
+        if (f == Vector3.zero)
+        {
+            f = Camera.main.transform.up;
+            f.y=0.0f;
+        }
+        f.Normalize();
+
+        Vector3 r = Camera.main.transform.right;
+        r.y = 0.0f;
+        r.Normalize();
+
         rb.AddForce(250 * Time.deltaTime * //new Vector3(moveValue.x,0,moveValue.y));
             (
-               Camera.main.transform.right*moveValue.x+
-               Camera.main.transform.forward*moveValue.y
+               r*moveValue.x+
+               f*moveValue.y
             ));
       /*  Vector2 axisValue = new Vector2(
             Input.GetAxis("Horizontal"),
